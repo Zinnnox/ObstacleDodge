94ee39baefbc6c0f57d6a8c734ad77297607c072
Cristian Montesinos Melara <zinnnox@outlook.com>
2024-12-08 13:54:19 -0500

    update, no changes

--- Assets/Scripts/TestingScripts/SettingsMenu.cs
+++ Assets/Scripts/TestingScripts/SettingsMenu.cs
@@ -13,26 +13,33 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField] TMP_Dropdown resolutionDropdown;
     [SerializeField] InputAction pauseMenuAction;
     bool isPaused = false;
-    private void Start() 
+    private void Start()
     {
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         int currentResolutionIndex = 0;
+        currentResolutionIndex = GetPossibleResolutionsForDisplay(options, currentResolutionIndex);
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    private int GetPossibleResolutionsForDisplay(List<string> options, int currentResolutionIndex)
+    {
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && 
+            if (resolutions[i].width == Screen.currentResolution.width &&
             resolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
         }
-        resolutionDropdown.AddOptions(options);    
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+
+        return currentResolutionIndex;
     }
 
     public void SetVolume(float volume)
